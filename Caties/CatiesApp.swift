2684d3a2a6cb7e7f404209b0d132309649953d9d
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-16 18:48:28 +0530

    Initial Commit

---
+++ Caties/CatiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatiesApp.swift
+//  Caties
+//
+//  Created by user208584 on 12/16/21.
+//
+
+import SwiftUI
+
+@main
+struct CatiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
