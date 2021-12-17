ca3a0cdf910e1e9db847adc91cc14b596dff14a7
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-17 18:39:13 +0530

    Added BreedImage Model

---
+++ Caties/Model/BreedImage.swift
@@ -0,0 +1,16 @@
+//
+//  BreedImage.swift
+//  Caties
+//
+//  Created by ASHISH LANGHE on 12/17/21.
+//
+
+import Foundation
+
+
+struct BreedImage: Codable {
+    let height: Int
+    let id: String
+    let url: String
+    let width: Int
+}
---
+++ Caties/Model/CatBreed.swift
@@ -0,0 +1,9 @@
+//
+//  CatBreed.swift
+//  Caties
+//
+//  Created by ASHISH LANGHE on 12/17/21.
+//
+
+import Foundation
+
