645823b25509360db7785e1a4d5019c4cfee7b8b
Ashish Langhe <95478770+Ashish-Langhe@users.noreply.github.com>
2021-12-18 23:47:06 +0530

    Added Breed entity model

---
+++ Caties/Model/Breed.swift
@@ -0,0 +1,19 @@
+//
+//  Breed.swift
+//  Caties
+//
+//  Created by user208584 on 12/18/21.
+//
+
+import Foundation
+
+struct Breed: Codable, CustomStringConvertible {
+    
+    let id: String
+    let name: String
+    let temperament: String
+    let breedExplanation: String
+    let energyLevel: Int
+    let isHairLess: Bool
+    let image: BreedImage
+}
